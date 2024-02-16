d71df368479b40e226a15ec998373d15c9e1189e
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-02-16 10:48:09 -0500

    Summary

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import { Routes, Route} from 'react-router-dom'
 import {Layout} from './components/Layout'
+import {Sidebar}  from './components/Sidebar'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Sidebar />} />
       </Routes>
     </>
   )
