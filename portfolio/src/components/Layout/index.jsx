44133ffe7106acd522cc66d69feeebc1c19340e3
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-02-16 11:16:29 -0500

    Summary

--- portfolio/src/components/Layout/index.jsx
+++ portfolio/src/components/Layout/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Sidebar } from '../Sidebar'
 import './index.scss'
 
 export const Layout = () => {
     
   return (
-    <div>Hello</div>
+    <>
+      <Sidebar />
+    </>
   )
 }
--- portfolio/src/components/Sidebar/index.jsx
+++ portfolio/src/components/Sidebar/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './index.scss'
+import { Link } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 
