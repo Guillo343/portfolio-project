76689f333297867a4f8c106c74dce169ff80c780
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-02-19 13:44:47 -0500

    Summary

---
+++ portfolio/src/components/Home/Logo/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+import LogoS from '../../../assets/images/logo-s.png'
+
+export const Logo = () => {
+
+  return (
+    <div className='logo-container'>
+        <img  className='solid-logo' src={LogoS} alt='S'/>
+        <svg width='559pt' height='897pt' version='1.0' viewBox='0 0 559 897' xmlns='http://www.w3.org/200/svg'
+        
+        />
+    </div>
+  )
+}
--- portfolio/src/components/Home/index.jsx
+++ portfolio/src/components/Home/index.jsx
@@ -3,6 +3,7 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
 import { AnimatedLetters } from '../AnimatedLetters'
+import { Logo } from './Logo'
 
 export const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -36,6 +37,7 @@ export const Home = () => {
             <h2>FrontEnd Developer </h2>
             <Link to='/contact' className='flat-button'> Contact Me </Link>
         </div>
+        <Logo />
     </div>
   )
 }
