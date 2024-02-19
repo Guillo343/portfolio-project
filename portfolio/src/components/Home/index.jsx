a3959ca1dc688573d9141983c1697d9db06663f3
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-02-19 11:44:53 -0500

    Summary

--- portfolio/src/components/Home/index.jsx
+++ portfolio/src/components/Home/index.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import { AnimatedLetters } from '../AnimatedLetters'
 
 export const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['p', 'i', 'd', 'e', 'r', 'm', 'a', 'n'];
+  const jobArray = ['w', 'e', 'b' , '' ,'d','e','v','e','l', 'o', 'p','e','r', '.']
+
   return (
     <div className='container home-page'>
         <div className="text-zone">
             <h1>Hi, <br /> I'm 
                 <img src={LogoTitle} alt="logo" />
-                piderman
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
                 <br />
                 web developer
             </h1>
